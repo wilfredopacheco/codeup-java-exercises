9a07bb4dc5bc6fd3670b16709da825e7854e2e68
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-09-24 16:24:10 -0500

    changed exit code to respond more accurately

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -9,51 +9,57 @@ public class MoviesApplication {
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         boolean exit = false;
         int i;
-        System.out.println("\nWhat would you like to do?\n");
-        System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n"
-                + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n"
-                + "5 - view movies in the scifi category\n");
-        System.out.println("Enter your choice:");
-        String input = scanner.next();
-        Movie[] movie = MoviesArray.findAll();
-
-        if (input.equals("0")) {
-            System.out.println("Goodbye!");
-            System.exit(0);
-        }
-        if (input.equals("1")) {
-            for (i = 0; i < movie.length; i++){
-                System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
-            }
-        }
-        if (input.equals("2")) {
-            for (i = 0; i < movie.length; i++) {
-                if (movie[i].getCategory().equals("animated")) {
+        String input;
+        do {
+            System.out.println("\nWhat would you like to do?\n");
+            System.out.println("0 - exit\n" + "1 - view all movies\n" + "2 - view movies in the animated category\n"
+                    + "3 - view movies in the drama category\n" + "4 - view movies in the horror category\n"
+                    + "5 - view movies in the scifi category\n");
+            System.out.println("Enter your choice:");
+            input = scanner.next();
+            Movie[] movie = MoviesArray.findAll();
+//
+//        if (!input.equals("0")) {
+//            System.out.println("Goodbye!");
+//            System.exit(0);
+//        }
+            if (input.equals("1")) {
+                for (i = 0; i < movie.length; i++) {
                     System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
                 }
             }
-        }
-        if (input.equals("3")) {
-            for (i = 0; i < movie.length; i++) {
-                if (movie[i].getCategory().equals("drama")) {
-                    System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+            if (input.equals("2")) {
+                for (i = 0; i < movie.length; i++) {
+                    if (movie[i].getCategory().equals("animated")) {
+                        System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+                    }
                 }
             }
-        }
-        if (input.equals("4")) {
-            for (i = 0; i < movie.length; i++) {
-                if (movie[i].getCategory().equals("horror")) {
-                    System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+            if (input.equals("3")) {
+                for (i = 0; i < movie.length; i++) {
+                    if (movie[i].getCategory().equals("drama")) {
+                        System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+                    }
                 }
             }
-        }
-        if (input.equals("5")) {
-            for (i = 0; i < movie.length; i++) {
-                if (movie[i].getCategory().equals("scifi")) {
-                    System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+            if (input.equals("4")) {
+                for (i = 0; i < movie.length; i++) {
+                    if (movie[i].getCategory().equals("horror")) {
+                        System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+                    }
                 }
             }
-        }
+            if (input.equals("5")) {
+                for (i = 0; i < movie.length; i++) {
+                    if (movie[i].getCategory().equals("scifi")) {
+                        System.out.println(movie[i].getName() + " -- " + movie[i].getCategory());
+                    }
+                }
+            }
+//            System.out.println("Please enter a number from the menu.");
+        } while (!input.equals("0"));
+        System.out.println("Goodbye!");
+        System.exit(0);
 
 
     }
