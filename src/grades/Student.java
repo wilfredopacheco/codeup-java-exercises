1518cdc6cf2d11a6206dccc151445bd8d2d8e24b
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-09-26 15:16:45 -0500

    completion of the exercise part one

---
+++ src/grades/Student.java
@@ -0,0 +1,42 @@
+package grades;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student {
+    private String name;
+    private ArrayList <Integer> grades;
+
+    public Student(String name, ArrayList grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double average = 0;
+        for (int grade : grades){
+            average+= grade;
+        }
+        return average/grades.size();
+    }
+
+    public static void main(String[] args) {
+        ArrayList <Integer> myGrades = new ArrayList<>();
+        myGrades.add(90);
+        myGrades.add(80);
+        myGrades.add(70);
+
+
+        Student student = new Student("Wilfredo",myGrades);
+        System.out.println(student.getName() + " has an average of " + student.getGradeAverage());
+    }
+}
